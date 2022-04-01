a96b422e9bfda16464b1fbace63786dc6e12c133
j1feng <15122988733m@gmail.com>
2022-04-01 14:15:19 +0800

    Indentation

--- client.cpp
+++ client.cpp
@@ -13,32 +13,35 @@
 
 int main(int argc,char *argv[])
 {
-    if (argc!=3)
-    {
-        printf("Using:./client ip port\nExample:./client 127.0.0.1 5005\n\n"); return -1;
+    if (argc!=3){
+        printf("Using:./client ip port\nExample:./client 127.0.0.1 5005\n\n");
+        return -1;
     }
 
     // 第1步：创建客户端的socket。
     int sockfd;
-    if ( (sockfd = socket(AF_INET,SOCK_STREAM,0))==-1) { perror("socket"); return -1; }
+    if ((sockfd = socket(AF_INET,SOCK_STREAM,0))==-1){
+        perror("socket"); return -1;
+    }
 
     // 第2步：向服务器发起连接请求。
     struct hostent* h;
-    if ( (h = gethostbyname(argv[1])) == 0 )   // 指定服务端的ip地址。
-    { printf("gethostbyname failed.\n"); close(sockfd); return -1; }
+    if ((h = gethostbyname(argv[1])) == 0 ){   // 指定服务端的ip地址。
+        printf("gethostbyname failed.\n"); close(sockfd); return -1;
+    }
     struct sockaddr_in servaddr;
     memset(&servaddr,0,sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(atoi(argv[2])); // 指定服务端的通信端口。
     memcpy(&servaddr.sin_addr,h->h_addr,h->h_length);
-    if (connect(sockfd, (struct sockaddr *)&servaddr,sizeof(servaddr)) != 0)  // 向服务端发起连接清求。
-    { perror("connect"); close(sockfd); return -1; }
+    if (connect(sockfd, (struct sockaddr *)&servaddr,sizeof(servaddr)) != 0){  // 向服务端发起连接清求。
+        perror("connect"); close(sockfd); return -1;
+    }
 
     char buffer[1024];
 
     // 第3步：与服务端通信，发送一个报文后等待回复，然后再发下一个报文。
-    for (int ii=0;ii<3;ii++)
-    {
+    for (int ii=0;ii<3;ii++){
         int iret;
         memset(buffer,0,sizeof(buffer));
         sprintf(buffer,"这是第%d个超级女生，编号%03d。",ii+1,ii+1);
