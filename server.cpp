374b0d85a72dc0a62c50ff9d01e528bea465a0c6
j1feng <15122988733m@gmail.com>
2022-04-01 14:16:51 +0800

    Indentation

--- server.cpp
+++ server.cpp
@@ -20,7 +20,8 @@ int main(int argc,char *argv[])
     // 第1步：创建服务端的socket。
     int listenfd;
     if((listenfd = socket(AF_INET,SOCK_STREAM,0))==-1){
-        perror("socket"); return -1;
+        perror("socket");
+        return -1;
     }
 
     // 第2步：把服务端用于通信的地址和端口绑定到socket上。
@@ -31,12 +32,14 @@ int main(int argc,char *argv[])
     //servaddr.sin_addr.s_addr = inet_addr("192.168.190.134"); // 指定ip地址。
     servaddr.sin_port = htons(atoi(argv[1]));  // 指定通信端口。
     if (bind(listenfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) != 0 ){
-        perror("bind"); close(listenfd); return -1;
+        perror("bind"); close(listenfd);
+        return -1;
     }
 
     // 第3步：把socket设置为监听模式。
     if (listen(listenfd,5) != 0 ){
-        perror("listen"); close(listenfd); return -1;
+        perror("listen"); close(listenfd);
+        return -1;
     }
 
     // 第4步：接受客户端的连接。
@@ -52,15 +55,18 @@ int main(int argc,char *argv[])
         int iret;
         memset(buffer,0,sizeof(buffer));
         if((iret=recv(clientfd,buffer,sizeof(buffer),0))<=0){ // 接收客户端的请求报文。
-            printf("iret=%d\n",iret); break;
+            printf("iret=%d\n",iret);
+            break;
         }
         printf("接收：%s\n",buffer);
 
         strcpy(buffer,"ok");
         if((iret=send(clientfd,buffer,strlen(buffer),0))<=0){ // 向客户端发送响应结果。
-            perror("send"); break; }
-            printf("发送：%s\n",buffer);
+            perror("send");
+            break;
         }
+            printf("发送：%s\n",buffer);
+    }
 
     // 第6步：关闭socket，释放资源。
     close(listenfd); close(clientfd);
